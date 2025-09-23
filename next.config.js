716303c42b059dbbd6d56185bcb0fabab581352f
dnrev-arch <ctt.dnrev@gmail.com>
2025-09-23 12:33:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'sample-videos.com'
-    ],
-    unoptimized: true
-  }
-};
-module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
